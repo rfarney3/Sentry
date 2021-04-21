1b0cef9b778fec3bb380446811c52aecb8040478
rfarney3 <farneyr@yahoo.com>
2021-04-21 15:08:50 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  bool = true;
+  toggleBool() {
+    this.bool = !this.bool;
+  }
+
+  errorFunction() {
+    var fakeFuncs = [{ func: function () {} }];
+    fakeFuncs[1].func();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+Sentry.init({
+  dsn:
+    'https://9badecaae1634b3d80c30f52bbbbaa0a@o566014.ingest.sentry.io/5729170',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost', 'https://yourserver.io/api'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: '', component: AppComponent }]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
